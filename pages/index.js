e0b69c1b22fe89f0e34f5d7c3d26400a11b3221b
Vinisangy <51464589+Vinisangy@users.noreply.github.com>
2025-04-22 23:42:07 +0000

    voltando do teste

--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,11 @@
-import React from "react";
-import Head from "next/head";
-
 function Home() {
   return (
-    <>
-      <Head>
-        <style>{`
-          body {
-            background-color: #1a1a1a;
-            color: #b084d9;
-            font-family: 'Arial', sans-serif;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 100vh;
-            margin: 0;
-          }
-
-          h1 {
-            font-size: 2.5rem;
-            text-align: center;
-            padding: 20px;
-            border: 2px solid #b084d9;
-            border-radius: 10px;
-            background-color: #2a2a2a;
-          }
-        `}</style>
-      </Head>
-      <h1>Kim, se você tá lendo isso, me manda um abacate 🥑</h1>
-    </>
+    <h1>
+      {" "}
+      Desculpa a reforma, estamos em transtornos, em breve novidades!!!!!
+    </h1>
   );
 }
 
+//exportando uma função padrão do arquivo
 export default Home;
